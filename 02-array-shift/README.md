014c989586f595f33cfd31e7e0a5be74c5c735bf
Concross <cacrossley22@gmail.com>
2018-09-07 01:45:11 -0700

    define insertShiftArray, all tests pass. create readme

--- 02-array-shift/README.md
+++ 02-array-shift/README.md
@@ -1,8 +1,8 @@
 # Insert and shift middle index of array
-<!-- Short summary or background information -->
+Shifts all items after the middle index and inserts an item into the gap
 
 ## Challenge
-<!-- Description of the challenge -->
+Write a function called `insertShiftArray` which takes in an array and the value to be added. Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
 
 ## Solution
-<!-- Embedded whiteboard image -->
+<img src="./assets/array_shift.jpg" width="400">
---
+++ 02-array-shift/lib/array_shift.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = exports = {};
+
+exports.insertShiftArray = (arr, item) => {
+
+  if (_isArray(arr)) {
+    let middleIndex = Math.ceil(arr.length / 2);
+    _insertMiddleItem(arr, middleIndex, item);
+
+    return arr;
+  }
+
+  throw new TypeError('insertShiftArray requires an array as its first argument');
+}
+
+function _isArray(arr) {
+  return arr instanceof Array;
+}
+
+function _insertMiddleItem(arr, middleIndex, item) {
+  for (let i = arr.length - 1; i >= middleIndex; i--) {
+    arr[i + 1] = arr[i];
+
+  }
+  arr[middleIndex] = item;
+}
