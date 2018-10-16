2a1ac4067c967abe1fe9b96b20df424fb7b0284c
Concross <cacrossley22@gmail.com>
2018-10-15 20:13:48 -0700

    basic graph developed

--- graphs/__tests__/models/graph.test.js
+++ graphs/__tests__/models/graph.test.js
@@ -9,4 +9,41 @@ describe('Graph constructor tests', () => {
 
     expect(graph).toBeInstanceOf(Graph);
   });
+
+  test('new graphs should have an empty object of vertices', () => {
+    let graph = new Graph();
+
+    let actual = graph.vertices;
+    let expected = {};
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('Graph addVertex method tests', () => {
+
+  test('should check that at least a value was passed as a parameter', () => {
+    let graph = new Graph();
+
+    expect(() => {
+      graph.addVertex();
+    }).toThrow();
+  });
+
+  test('should update the graph vertices with the new vertex', () => {
+    let graph = new Graph();
+
+    graph.addVertex('a');
+
+    expect(graph.vertices).toHaveProperty('a');
+  });
+
+  test('vertices should have neighbors value if added', () => {
+    let graph = new Graph();
+
+    graph.addVertex('a', ['b', 'c', 'd']);
+
+    expect(graph.vertices['a']).toEqual(['b', 'c', 'd']);
+    expect(graph.vertices['a']).not.toEqual(['d', 'c', 'b']);
+  });
 });
--- graphs/lib/models/graph.js
+++ graphs/lib/models/graph.js
@@ -4,6 +4,15 @@ class Graph {
   constructor() {
     this.vertices = {};
   }
+
+  addVertex(value, neighbors) {
+
+    if (!value) {
+      throw new Error('Input Error: addVertex(value [, neighbors]) requires a value');
+    }
+
+    this.vertices[value] = neighbors;
+  }
 }
 
 module.exports = Graph;
