733ebddcd4c6ee0f448223398353b62a2a15cf36
Concross <cacrossley22@gmail.com>
2018-10-19 16:28:19 -0700

    hash method tested

--- hash-tables/__tests__/hashTable.test.js
+++ hash-tables/__tests__/hashTable.test.js
@@ -1,36 +1,63 @@
 const HashTable = require('../lib/hashTable');
 
 describe('HashTable tests', () => {
-  test('an instance created by HashTable should be of type HashTable', () => {
-    const hashTable = new HashTable(5);
+  describe('constructor', () => {
+    test('an instance created by HashTable should be of type HashTable', () => {
+      const hashTable = new HashTable(5);
 
-    expect(hashTable).toBeInstanceOf(HashTable);
-  });
+      expect(hashTable).toBeInstanceOf(HashTable);
+    });
 
-  test('constructor should create a bucket storage array of the given length', () => {
-    const bucketCount = 5;
-    const hashTable = new HashTable(bucketCount);
+    test('constructor should create a bucket storage array of the given length', () => {
+      const bucketCount = 5;
+      const hashTable = new HashTable(bucketCount);
 
-    let actual = hashTable.buckets.length;
+      let actual = hashTable.buckets.length;
 
-    expect(actual).toBe(bucketCount);
-  });
+      expect(actual).toBe(bucketCount);
+    });
 
-  test('bucket should contain null values when instantiated', () => {
-    const bucketCount = 5;
-    const hashTable = new HashTable(bucketCount);
+    test('bucket should contain null values when instantiated', () => {
+      const bucketCount = 5;
+      const hashTable = new HashTable(bucketCount);
 
-    hashTable.buckets.forEach(bucket => {
-      expect(bucket).toBeNull();
+      hashTable.buckets.forEach(bucket => {
+        expect(bucket).toBeNull();
+      });
     });
   });
 
-  test('hash method tests', () => {
-    const hashTable = new HashTable(5);
-    const hash = hashTable.hash;
-    let actual = hash('connor');
-    let expected = 0;
+  describe('hash method', () => {
+    test('hash should throw an error if not passed a string', () => {
+      const hashTable = new HashTable(5);
 
-    expect(actual).toBe(expected);
+      const hash = hashTable.hash;
+      const badArgs = [[], {}, false, 0, undefined, null];
+
+      badArgs.forEach(arg => {
+        expect(() => {
+          hash(arg);
+        }).toThrow();
+      });
+    });
+
+    test('hash should return 0 if passed key of "connor"', () => {
+      const hashTable = new HashTable(5);
+      let actual = hashTable.hash('connor');
+      let expected = 0;
+
+      expect(actual).toBe(expected);
+    });
+
+    test('hash should always return 0 if passed key of "connor"', () => {
+      const hashTable = new HashTable(5);
+
+      for (let i = 0; i < 2; i++) {
+        let actual = hashTable.hash('connor');
+        let expected = 0;
+
+        expect(actual).toBe(expected);
+      }
+    });
   });
 });
--- hash-tables/lib/hashTable.js
+++ hash-tables/lib/hashTable.js
@@ -3,6 +3,12 @@ class HashTable {
     this.bucketCount = bucketCount;
     this.buckets = Array(this.bucketCount).fill(null);
   }
+
+  hash(key) {
+    if (typeof key !== 'string') { throw new Error('hash(key) requires a string representing a key'); }
+
+    return key.split('').reduce((acc, char) => acc * char.charCodeAt(0), 1) % this.buckets.length;
+  }
 }
 
 module.exports = HashTable;
