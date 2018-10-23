392aca4e88785d056ff5f582ab6cee924096ec40
Concross <cacrossley22@gmail.com>
2018-10-22 22:48:31 -0700

    fix for punctuation

--- challenges/repeated-word/__tests__/repeatedWord.test.js
+++ challenges/repeated-word/__tests__/repeatedWord.test.js
@@ -10,4 +10,37 @@ describe('repeatedWord teste', () => {
       }).toThrow();
     });
   });
+
+  test('should not throw an error if input is a string', () => {
+    const goodArgs = ['this is such a great book!', 'test', ''];
+
+    goodArgs.forEach(arg => {
+      expect(() => {
+        repeatedWord(arg);
+      }).not.toThrow('Input Error: input must be a string');
+    });
+  });
+
+  test('should return the first repeated word in a book', () => {
+    const book = 'Once upon a time, there was a brave princess who...';
+
+    const actual = repeatedWord(book);
+
+    expect(actual).toBe('a');
+  });
+
+  test('should return the first repeated word in test books', () => {
+    const book = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...';
+
+    const actual = repeatedWord(book);
+
+    expect(actual).toBe('it');
+  });
+
+  test('should return the first repeated word in test books', () => {
+    const book = 'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...';
+
+    const actual = repeatedWord(book);
+    expect(actual).toBe('summer');
+  });
 });
--- challenges/repeated-word/lib/repeatedWord.js
+++ challenges/repeated-word/lib/repeatedWord.js
@@ -2,4 +2,18 @@ module.exports = (book) => {
   if (typeof book !== 'string') {
     throw new Error('Input Error: input must be a string');
   }
+  // remove unnecessary punctuation. 
+  const parsedBook = book.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+
+  const words = parsedBook.split(' ');
+  const result = {};
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i].toLowerCase();
+    if (result[word]) {
+      return word;
+    }
+
+    result[word] = 1;
+  }
 };
