48fde1f10a48428d71cb230faa522af491ca0d50
Concross <cacrossley22@gmail.com>
2018-11-01 12:24:57 -0700

    array_reverse refactored

--- algorithms/array-reverse/array_reverse.js
+++ algorithms/array-reverse/array_reverse.js
@@ -1,12 +1,11 @@
 'use strict';
 
-module.exports = exports = {};
-
-exports.reverseArray = (arr) => {
-  let reversedArr = arr;
+module.exports = (arr) => {
+  const reversedArr = arr;
 
   for (let i = 0, j = (arr.length - 1); i < j; i++ , j--) {
     [reversedArr[i], reversedArr[j]] = [reversedArr[j], reversedArr[i]];
   }
+
   return reversedArr;
-}
+};
