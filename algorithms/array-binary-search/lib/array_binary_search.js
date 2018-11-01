e0497c92a0510cc2dd578014e60844d8e54505b3
Concross <cacrossley22@gmail.com>
2018-11-01 12:19:56 -0700

    array_binary_search refactored

--- algorithms/array-binary-search/lib/array_binary_search.js
+++ algorithms/array-binary-search/lib/array_binary_search.js
@@ -1,18 +1,23 @@
 'use-strict';
 
-module.exports = exports = (arr, target) => {
+module.exports = (arr, target) => {
   let begin = 0;
   let end = arr.length - 1;
   let mid;
+
   while (begin <= end) {
     mid = Math.floor((begin + end) / 2);
+
     if (target > arr[mid]) {
       begin = mid + 1;
+
     } else if (target < arr[mid]) {
       end = mid - 1;
+
     } else {
       return mid;
     }
   }
+
   return -1;
 };
